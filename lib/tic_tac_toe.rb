10d76492b1e5f2f1d2043d9ffa7fb275883ba89d
Andre King <andreking1031@gmail.com>
2020-08-28 15:27:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,5 +28,6 @@ def move(index, current player = "X")
   @board[index] = current_player
 end
 def position_taken?(index)
-  !(@board[index].nil? || @board[index] == )
+  !(@board[index].nil? || @board[index] == " ")
+end
   
